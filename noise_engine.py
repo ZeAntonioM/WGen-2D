0b82ff2e1ecc367474dc088a475c76f722ba6732
Haeherfeder333 <137832322+Haeherfeder333@users.noreply.github.com>
2026-01-30 12:20:02 +0100

    cosmetic minor touch ups

--- noise_engine.py
+++ noise_engine.py
@@ -6,15 +6,16 @@ from pyfastnoiselite.pyfastnoiselite import FastNoiseLite, NoiseType, FractalTyp
 
 class NoiseEngine:
 
-    """
-    There should only be one instance of this class.
-    This class is resposible for generating noise values for terrain generation.
-    FastnoiseLite is used for noise generation, and the main parameters are set at initialization. 
-    
-    @param seed: The seed for the noise generation. Should be randomized for different worlds, or given by the user for consistent worlds.
-    @param frequency: The frequency of the noise.
-    """
-    def __init__(self, seed, frequency=0.01):
+    def __init__(self, seed, frequency=0.01, fractal_octaves=5):
+        """
+        There should only be one instance of this class.
+        This class is resposible for generating noise values for terrain generation.
+        FastNoiseLite is used for noise generation, and the main parameters are set at initialization. 
+        
+        @param seed: The seed for the noise generation. Should be randomized for different worlds, or given by the user for consistent worlds.
+        @param frequency: The frequency of the noise.
+        """
+        
         self.chunk_size = 32
         
         # initialize FastNoiseLite with seed
@@ -22,26 +23,25 @@ class NoiseEngine:
         
         # Following the original Paper, we should set the Noise type to simplex, and Fractal type to FBm.
         self.noise.noise_type = NoiseType.NoiseType_OpenSimplex2
-        #help(FractalType)
         self.noise.fractal_type = FractalType.FractalType_FBm
-        #self.noise.SetFractalType(FractalType.FastNoiseLite_FractalFBm)
         
         # The number of octaves determines how much detail is added to the noise.
-        self.noise.fractal_octaves = 5
+        self.noise.fractal_octaves = fractal_octaves
         
         # Depending on the desired terrain, we should set the frequency appropriately.
         # A lower frequency created larger biomes, while a higher frequency creates smaller biomes.
         self.noise.frequency = frequency
         
         
-    """
-    Generate a height map for a given chunk using FastNoiseLite.
     
-    @param chunk_x: The x coordinate of the chunk. Must me the "world" coordinate, not the local chunk coordinate.
-    @param chunk_y: The y coordinate of the chunk. Must me the "world" coordinate, not the local chunk coordinate.
-    @return: A 2D numpy array representing the height map for the chunk.
-    """    
     def get_noise_height_map(self, chunk_x, chunk_y):
+        """
+        Generate a height map for a given chunk using FastNoiseLite.
+        
+        @param chunk_x: The x coordinate of the chunk. Must me the "world" coordinate, not the local chunk coordinate.
+        @param chunk_y: The y coordinate of the chunk. Must me the "world" coordinate, not the local chunk coordinate.
+        @return: A 2D numpy array representing the height map for the chunk.
+        """
         
         # We create a height map of size chunk_size x chunk_size, as each chunk is of that size. Built it in a scalable way so that we can change chunk size later if needed.
         height_map = np.zeros((self.chunk_size, self.chunk_size), dtype=np.float32)
@@ -60,5 +60,4 @@ class NoiseEngine:
                 height_map[x, y] = (noise_value + 1) / 2.0
 
         return height_map
-                
-                
+
