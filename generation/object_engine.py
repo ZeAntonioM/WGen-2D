ec027e80bcdcfd6e199f94cfa1f2957a0c3f430a
FranciscoCardoso913 <franciscocardoso.3003@gmail.com>
2026-02-02 15:56:45 +0100

    Change biomes object rules to config file

--- generation/object_engine.py
+++ generation/object_engine.py
@@ -1,6 +1,7 @@
 import numpy as np
 import settings
 from generation.biome_placement import Biome
+import config.object_rules as rules
 
 class WorldObject:
     NONE = 0
@@ -19,6 +20,9 @@ class ObjectEngine:
         self.global_seed = global_seed
         
     def generate_object_map(self, chunk_x, chunk_y, biome_data):
+        """
+        Returns a 32x32 array of integers representing objects.
+        """
         cx = int(settings.CHUNK_SIZE.x)
         cy = int(settings.CHUNK_SIZE.y)
         
@@ -37,43 +41,47 @@ class ObjectEngine:
             else:
                 return (biome_data == biome_enum.value).astype(np.float32)
         
+        # --- RULES ---
+        
         # Rule 1: Temperate Forests 
         w_forest = get_weight(Biome.TEMPERATE_FOREST)
-        object_map[chance_map < (0.10 * w_forest)] = WorldObject.TREE
+        object_map[chance_map < (rules.DENSITY_FOREST_TREE * w_forest)] = WorldObject.TREE
         
-        # Rule 2: Rainforests 
+        # Rule 2: Rainforests (Combined Temperate + Tropical)
         w_rainforest = get_weight(Biome.TEMPERATE_RAINFOREST) + \
                        get_weight(Biome.TROPICAL_RAINFOREST)
-        object_map[chance_map < (0.20 * w_rainforest)] = WorldObject.TREE
+        object_map[chance_map < (rules.DENSITY_RAINFOREST_TREE * w_rainforest)] = WorldObject.TREE
         
         # Rule 3: Deserts (Cactus)
         w_desert = get_weight(Biome.SUBTROPICAL_DESERT)
-        object_map[chance_map < (0.02 * w_desert)] = WorldObject.CACTUS
+        object_map[chance_map < (rules.DENSITY_DESERT_CACTUS * w_desert)] = WorldObject.CACTUS
         
         # Rule 4: Tropical (Palms)
         w_tropical = get_weight(Biome.TROPICAL_SEASONAL_FOREST)
-        object_map[chance_map < (0.05 * w_tropical)] = WorldObject.PALM
+        object_map[chance_map < (rules.DENSITY_TROPICAL_PALM * w_tropical)] = WorldObject.PALM
 
         # Rule 5: Taiga (Snow Trees)
         w_taiga = get_weight(Biome.TAIGA)
-        object_map[chance_map < (0.10 * w_taiga)] = WorldObject.SNOW_TREE
+        object_map[chance_map < (rules.DENSITY_TAIGA_SNOW_TREE * w_taiga)] = WorldObject.SNOW_TREE
 
         # Rule 6: Rocks in High Mountains
         w_tundra = get_weight(Biome.TUNDRA)
-        object_map[chance_map < (0.04 * w_tundra)] = WorldObject.ROCK
+        object_map[chance_map < (rules.DENSITY_TUNDRA_ROCK * w_tundra)] = WorldObject.ROCK
 
         # Rule 7: Wildflowers in Temperate Grassland
         w_grassland = get_weight(Biome.TEMPERATE_GRASSLAND)
-        object_map[chance_map < (0.005 * w_grassland)] = WorldObject.FLOWER
+        object_map[chance_map < (rules.DENSITY_GRASSLAND_FLOWER * w_grassland)] = WorldObject.FLOWER
 
         # Rule 8: Dead Bushes and Rocks in Desert 
-        object_map[chance_map > (1.0 - (0.02 * w_desert))] = WorldObject.DEAD_BUSH
-        object_map[chance_map > (1.0 - (0.01 * w_desert))] = WorldObject.ROCK
+        object_map[chance_map > (1.0 - rules.DENSITY_DESERT_DEADBUSH * w_desert)] = WorldObject.DEAD_BUSH
+        
+        # For Desert rocks
+        object_map[chance_map > (1.0 - rules.DENSITY_DESERT_ROCK * w_desert)] = WorldObject.ROCK
 
         # Rule 9: Mushrooms in Rainforests 
-        object_map[chance_map > (1.0 - (0.01 * w_rainforest))] = WorldObject.MUSHROOM
+        object_map[chance_map > (1.0 - rules.DENSITY_RAINFOREST_MUSHROOM * w_rainforest)] = WorldObject.MUSHROOM
 
         # Rule 10: Berry Bushes in Taiga 
-        object_map[chance_map > (1.0 - (0.10 * w_taiga))] = WorldObject.BERRY_BUSH
+        object_map[chance_map > (1.0 - rules.DENSITY_TAIGA_BERRY * w_taiga)] = WorldObject.BERRY_BUSH
         
         return object_map
--- settings.py
+++ settings.py
@@ -4,8 +4,8 @@ import warnings
 
 # Import everything from the split files
 # This allows 'import settings' to work exactly as it did before
-from app_settings import *
-from simulation_settings import *
+from config.app_settings import *
+from config.simulation_settings import *
 
 # --- VALIDATION CHECKS ---
 # We keep these here to ensure the combined settings are valid.
